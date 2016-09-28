a0c0324b9bd14e3a8b2e5cde0b75d4c1b3208bef
Xinghun Meng <xinghunm@curacloudcorp.com>
2016-09-28 18:58:17 +0800

    Delegate Design Pattern

--- IOS-Design-Patterns/IOS-Design-Patterns/MainViewController.m
+++ IOS-Design-Patterns/IOS-Design-Patterns/MainViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "MainViewController.h"
+#import "LibraryAPI.h"
+#import "Album+TableRepresentation.h"
 
-@interface MainViewController ()
+@interface MainViewController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    UITableView *dataTable;
+    NSArray *allAlbums;
+    NSDictionary *currentAlbumData;
+    int currentAlbumIndex;
+}
 
 @end
 
@@ -17,7 +25,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor colorWithRed:0.76f green:0.81f blue:0.87f alpha:1];;
+    currentAlbumIndex = 0;
+    allAlbums = [[LibraryAPI sharedInstance] getAlbums];
+    // the uitableview that presents the album data
+    dataTable = [[UITableView alloc]initWithFrame:CGRectMake(0, 120, self.view.frame.size.width, self.view.frame.size.height-120) style:UITableViewStyleGrouped];
+    dataTable.delegate = self;
+    dataTable.dataSource = self;
+    [self.view addSubview:dataTable];
+    [self showDataForAlbumAtIndex:currentAlbumIndex];
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [currentAlbumData[@"titles"] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+        cell.textLabel.text = currentAlbumData[@"titles"][indexPath.row];
+        cell.detailTextLabel.text = currentAlbumData[@"values"][indexPath.row];
+    }
+    return cell;
+}
+- (void)showDataForAlbumAtIndex:(int)albumIndex {
+    if (albumIndex <allAlbums.count) {
+        Album *album= allAlbums[albumIndex];
+        currentAlbumData = [album tr_tableRepresentation];
+    } else {
+        currentAlbumData = nil;
+    }
+    [dataTable reloadData];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
