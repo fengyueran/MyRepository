9be35b191b6d391d40c658ead6a3375f0ff118b3
fengyueran <316032603@qq.com>
2016-09-29 13:50:53 +0800

    uiscroview

--- UIScroview/UIScroview/ViewController.m
+++ UIScroview/UIScroview/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -17,6 +18,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //建立内容视图,imageView的frame.origin以父scrollView为原点(0,0)，imageView.origin为(0,0).
+    UIImageView * imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"splash"]];
+    
+    // 滚动条样式
+    self.scrollView.indicatorStyle = UIScrollViewIndicatorStyleWhite;
+    
+    // 是否显示水平滚动条
+    self.scrollView.showsHorizontalScrollIndicator = YES;
+    // 是否显示垂直滚动条
+    self.scrollView.showsVerticalScrollIndicator =YES;
+    
+    //告诉UIScrollView所有内容的尺寸,也就是告诉它滚动的范围,包含当前scroview的大小。
+    self.scrollView.contentSize = imageView.bounds.size;
+    
+    //contentOffset默认为(0,0)，即只显示图片左上角，contentOffset为偏离默认状态距离的度量。
+    self.scrollView.contentOffset = CGPointMake(200, 200);
+    //scroview的、frame.origin以父view为原点(0,0).
+    [self.scrollView addSubview:imageView];
 }
 
 - (void)didReceiveMemoryWarning {
