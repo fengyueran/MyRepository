bcc342d839194094372a2fc0d8c1f8b54ec37466
xinghun meng <xinghunm@curacloudcorp.com>
2016-10-08 18:35:06 +0800

    init the CoreData repository

---
+++ CoreData/CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by intern08 on 10/8/16.
+//  Copyright © 2016 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
