b8c0898ce73268d1679d1e5817c3451755be8913
xinghun meng <xinghunm@curacloudcorp.com>
2016-10-10 12:48:21 +0800

    add context

--- CoreData/CoreData/ViewController.m
+++ CoreData/CoreData/ViewController.m
@@ -7,12 +7,64 @@
 //
 
 #import "ViewController.h"
+#import <CoreData/CoreData.h>
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+#pragma mark - ----- CoreData简单创建流程 ------
+
+/**
+ 模型文件操作
+ 1.1 创建模型文件，后缀名为.xcdatamodeld。创建模型文件之后，可以在其内部进行添加实体等操作(用于表示数据库文件的数据结构)
+ 1.2 添加实体(表示数据库文件中的表结构)，添加实体后需要通过实体，来创建托管对象类文件。
+ 1.3 添加属性并设置类型，可以在属性的右侧面板中设置默认值等选项。(每种数据类型设置选项是不同的)
+ 1.4 创建获取请求模板、设置配置模板等。
+ 1.5 根据指定实体，创建托管对象类文件(基于NSManagedObject的类文件)
+ 
+ 
+ 实例化上下文对象
+ 2.1 创建托管对象上下文(NSManagedObjectContext)
+ 2.2 创建托管对象模型(NSManagedObjectModel)
+ 2.3 根据托管对象模型，创建持久化存储协调器(NSPersistentStoreCoordinator)
+ 2.4 关联并创建本地数据库文件，并返回持久化存储对象(NSPersistentStore)
+ 2.5 将持久化存储协调器赋值给托管对象上下文，完成基本创建。
+ */
+
+#pragma mark - ----- Create CoreData Context ------
+
+/**
+创建上下文对象
+*/
+
+- (NSManagedObjectContext *)contextWithModelName:(NSString *)modelName {
+    // 创建上下文对象，并发队列设置为主队列
+    NSManagedObjectContext *context = [[NSManagedObjectContext alloc]initWithConcurrencyType:NSMainQueueConcurrencyType];
+    
+    // 创建托管对象模型，并使用Company.momd路径当做初始化参数
+    NSURL *modelPath = [[NSBundle mainBundle]URLForResource:modelName withExtension:@"momd"];
+    NSManagedObjectModel *model = [[NSManagedObjectModel alloc]initWithContentsOfURL:modelPath];
+    
+     // 创建持久化存储调度器
+    NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc]initWithManagedObjectModel:model];
+    
+    // 创建并关联SQLite数据库文件，如果已经存在则不会重复创建
+    NSString *dataPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).lastObject;
+    dataPath = [dataPath stringByAppendingFormat:@"/%@.sqlite",modelName];
+    [coordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:[NSURL URLWithString:dataPath] options:nil error:nil];
+    // 上下文对象设置属性为持久化存储器
+    context.persistentStoreCoordinator =coordinator;
+    
+    return context;
+    
+}
+
+- (IBAction)reverseRelationshipsAdd:(UIButton *)sender {
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
