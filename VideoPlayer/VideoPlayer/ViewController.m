a24b0b550b7c1d2eddae4138740e543a372b760e
xinghun meng <xinghunm@curacloudcorp.com>
2016-11-18 14:07:37 +0800

    add avplayerController

--- VideoPlayer/VideoPlayer/ViewController.m
+++ VideoPlayer/VideoPlayer/ViewController.m
@@ -14,6 +14,7 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) AVPlayer *player;
+@property (nonatomic, strong) AVPlayerViewController *playerVC;
 @property (nonatomic, strong) MPMoviePlayerController *mpMovieplayer;
 @property (nonatomic, strong) MPMoviePlayerViewController *mpMovieViewplayer;
 
@@ -23,15 +24,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //1.AVPlayer
-    AVPlayer *player = self.player;
+    //1.AVPlayer,最简单的视频播放器
+//    AVPlayer *player = self.player;
     
-    //2.MPMoviePlayerController,已弃用
+     //2.AVPlayerViewController，有基本的播放控件
+    AVPlayerViewController *playerVC = self.playerVC;
+    playerVC.view.frame = CGRectMake(50, 50, 300, 350);
+    [self.view addSubview:playerVC.view];
+//     playerVC.player.externalPlaybackVideoGravity = AVLayerVideoGravityResizeAspectFill;//这个属性和图片填充试图的属性类似，也可以设置为自适应试图大小。
+
+    //3.MPMoviePlayerController,已弃用
     //  MPMoviePlayerController *player = self.mpMovieplayer;
-    //3.MPMoviePlayerViewController，已弃用
+    
+    //4.MPMoviePlayerViewController，已弃用
     // [self presentMoviePlayerViewControllerAnimated:self.mpMovieViewplayer];
     
-    [player play];
+//    [player play];
     
     
 }
@@ -61,10 +69,12 @@
         //    AVLayerVideoGravityResizeAspectFill   充满的
         //    currentItem  获得当前播放的视频元素
         
+        [item addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+        
         _player = [AVPlayer playerWithPlayerItem:item];
         // 4.添加AVPlayerLayer
         AVPlayerLayer *layer = [AVPlayerLayer playerLayerWithPlayer:_player];
-        layer.frame = CGRectMake(0, 100, self.view.bounds.size.width, self.view.bounds.size.width* 9 / 16);
+//        layer.frame = CGRectMake(0, 100, self.view.bounds.size.width, self.view.bounds.size.width* 9 / 16);
         // 显示播放视频的视图层要添加到self.view的视图层上面
         [self.view.layer addSublayer:layer];
         
@@ -72,8 +82,44 @@
     return _player;
 }
 
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    switch ([change[@"new"]integerValue]) {
+        case 0:
+             NSLog(@"未知状态");
+            break;
+        case 1:{
+            NSLog(@"获得视频总时长  %f",CMTimeGetSeconds(self.player.currentItem.duration));//CMTime在下面会介绍
+            break;
+        }
+        case 2:{
+            NSLog(@"加载失败");
+            break;
+        }
+        default:
+            break;
+    }
+}
 
+- (AVPlayerViewController *)playerVC {
+    if (_playerVC == nil) {
+         //创建一个视频播放控制器
+        _playerVC = [[AVPlayerViewController alloc]init];
+        //获取视频URL（远程、本地视频URL都可以）
+//           NSURL *url = [NSURL URLWithString:@"http://v1.mukewang.com/19954d8f-e2c2-4c0a-b8c1-a4c826b5ca8b/L.mp4"];
+//        //根据URL创建播放曲目
+//        AVPlayerItem * item = [AVPlayerItem playerItemWithURL:url];
+//        //创建一个视频播放器
+//        AVPlayer *player = [AVPlayer playerWithPlayerItem:item];
+//        //创建一个视频播放图层
+//        AVPlayerLayer *playLayer = [AVPlayerLayer playerLayerWithPlayer:player];
+//         //将视频播放图层添加到父控件图层
+//        [_playerVC.view.layer addSublayer:playLayer];
+        //设置视频播放控制器的播放器为player
+        _playerVC.player = self.player;
 
+    }
+    return _playerVC;
+}
 
 
 
