cafe1f8c8a45b89c727ce7969999aa85f147c11c
xinghun meng <xinghunm@curacloudcorp.com>
2016-11-24 18:31:27 +0800

    init the repository

---
+++ VideoPlayer_ijkplayer/VideoPlayer_ijkplayerTests/VideoPlayer_ijkplayerTests.m
@@ -0,0 +1,39 @@
+//
+//  VideoPlayer_ijkplayerTests.m
+//  VideoPlayer_ijkplayerTests
+//
+//  Created by intern08 on 11/24/16.
+//  Copyright © 2016 snow. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VideoPlayer_ijkplayerTests : XCTestCase
+
+@end
+
+@implementation VideoPlayer_ijkplayerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
