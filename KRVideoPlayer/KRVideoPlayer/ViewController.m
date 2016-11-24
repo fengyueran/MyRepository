3404ca8d914d9a1197e669992318c96a39f4b741
xinghun meng <xinghunm@curacloudcorp.com>
2016-11-24 16:37:50 +0800

    add KRVideoPlayer

--- KRVideoPlayer/KRVideoPlayer/ViewController.m
+++ KRVideoPlayer/KRVideoPlayer/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "KRVideoPlayerController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) KRVideoPlayerController *videoController;
 @end
 
 @implementation ViewController
@@ -19,9 +20,30 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)playLocalVideo:(id)sender
+{
+    NSURL *videoURL = [[NSBundle mainBundle] URLForResource:@"150511_JiveBike" withExtension:@"mov"];
+    [self playVideoWithURL:videoURL];
+}
+
+- (IBAction)playRemoteVideo:(id)sender
+{
+    NSURL *videoURL = [NSURL URLWithString:@"http://krtv.qiniudn.com/150522nextapp"];
+    [self playVideoWithURL:videoURL];
+}
+
+- (void)playVideoWithURL:(NSURL *)url
+{
+    if (!self.videoController) {
+        CGFloat width = [UIScreen mainScreen].bounds.size.width;
+        self.videoController = [[KRVideoPlayerController alloc] initWithFrame:CGRectMake(0, 0, width, width*(9.0/16.0))];
+        __weak typeof(self)weakSelf = self;
+        [self.videoController setDimissCompleteBlock:^{
+            weakSelf.videoController = nil;
+        }];
+        [self.videoController showInWindow];
+    }
+    self.videoController.contentURL = url;
 }
 
 @end
